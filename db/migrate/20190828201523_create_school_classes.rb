a475bca7c297beec303d5e3f5a86cf5d86fc3fe9
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-08-28 20:16:03 +0000

    Automatically backed up by Learn

--- db/migrate/20190828201523_create_school_classes.rb
+++ db/migrate/20190828201523_create_school_classes.rb
@@ -2,7 +2,8 @@ class CreateSchoolClasses < ActiveRecord::Migration[5.0]
   def change
     create_table :school_classes do |t|
       t.string :title
-      t.integer
+      t.integer :room_number
+      t.timestamps null: false
     end
   end
 end
