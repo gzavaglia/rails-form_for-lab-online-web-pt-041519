f4093366bb0a4df2ffb1d48ecb96e18aa39e6602
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-08-28 20:13:00 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  
+  resources :students, only: [:index, :new, :create, :show, :edit, :update]
+  resources :school_classes, only: [:index, :new, :create, :show, :edit, :update]
 end
